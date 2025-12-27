7328b8809e1bedc58db20f800e4525dc4133bf76
yasas banuka <yixbinfo@gmail.com>
2025-12-27 12:17:26 +0530

    fix(utilities): proper pluralization and human readable text

--- src/utilities/relativeTime.ts
+++ src/utilities/relativeTime.ts
@@ -7,6 +7,7 @@ export function relativeTime(timestamp: Date | number | string) {
         "timestamp is either null or undefined, please read documentation for more info",
     };
   }
+
   const timestampDate =
     timestamp instanceof Date ? timestamp : new Date(timestamp);
 
@@ -15,45 +16,58 @@ export function relativeTime(timestamp: Date | number | string) {
       error: "invalid timestamp, please read documentation for more info",
     };
   }
-  let humanTxt: string;
-  const afterResult = isAfter(timestamp);
 
+  let humanTxt: string;
+
+  const afterResult = isAfter(timestamp);
   if ("error" in afterResult) {
     return afterResult;
   }
 
-  const durationType = afterResult.isAfter ? afterResult : isBefore(timestamp);
-
-  if ("error" in durationType) {
-    return durationType;
+  const beforeResult = isBefore(timestamp);
+  if ("error" in beforeResult) {
+    return beforeResult;
   }
 
-  if (afterResult.isAfter) {
-    if (durationType.durationInSeconds < 60) {
-      humanTxt = `${Math.round(durationType.durationInSeconds)} second(s) ago`;
-    } else if (durationType.durationInMinutes < 60) {
-      humanTxt = `${Math.round(durationType.durationInMinutes)} minute(s) ago`;
-    } else if (durationType.durationInHours < 24) {
-      humanTxt = `${Math.round(durationType.durationInHours)} hour(s) ago`;
-    } else {
-      const months = Math.round(durationType.durationInDays / 30);
-      if (months < 12) {
-        humanTxt = `${months} month(s) ago`;
-      } else {
-        const years = Math.round(months / 12);
-        humanTxt = `${years} year(s) ago`;
-      }
-    }
+  const isFuture = afterResult.isAfter;
+  const duration = isFuture ? afterResult : beforeResult;
+
+  if (duration.durationInSeconds < 60) {
+    const v = Math.round(duration.durationInSeconds);
+    humanTxt = `${v} second${v === 1 ? "" : "s"} ${
+      isFuture ? "from now" : "ago"
+    }`;
+  } else if (duration.durationInMinutes < 60) {
+    const v = Math.round(duration.durationInMinutes);
+    humanTxt = `${v} minute${v === 1 ? "" : "s"} ${
+      isFuture ? "from now" : "ago"
+    }`;
+  } else if (duration.durationInHours < 24) {
+    const v = Math.round(duration.durationInHours);
+    humanTxt = `${v} hour${v === 1 ? "" : "s"} ${
+      isFuture ? "from now" : "ago"
+    }`;
+  } else if (duration.durationInDays < 30) {
+    const v = Math.round(duration.durationInDays);
+    humanTxt = `${v} day${v === 1 ? "" : "s"} ${isFuture ? "from now" : "ago"}`;
   } else {
-    return {
-      error: "invalid timestamp, please read documentation for more info",
-    };
+    const months = Math.round(duration.durationInDays / 30);
+    if (months < 12) {
+      humanTxt = `${months} month${months === 1 ? "" : "s"} ${
+        isFuture ? "from now" : "ago"
+      }`;
+    } else {
+      const years = Math.round(months / 12);
+      humanTxt = `${years} year${years === 1 ? "" : "s"} ${
+        isFuture ? "from now" : "ago"
+      }`;
+    }
   }
 
   return {
     timestamp: Date.parse(timestampDate.toString()),
     timestampDate,
     humanReadable: humanTxt,
-    isInFuture: afterResult.isAfter,
+    isInFuture: isFuture,
   };
 }
